4d58443263bf12325642064ffa1511e87a7c0523
Joel Varty <joel@agilitycms.com>
2021-05-26 10:54:30 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 
-import React, { useState, useEffect } from "react"
-import EditorJS from '@editorjs/editorjs';
-import Header from '@editorjs/header';
-import List from '@editorjs/list';
+import React, { useState, useEffect, useRef } from "react"
+import EditorJS from '@editorjs/editorjs'
+import Header from '@editorjs/header'
+import List from '@editorjs/list'
+import ImageTool from '@editorjs/image'
+import Quote from '@editorjs/quote'
+import Link from '@editorjs/link'
+import InlineCode from '@editorjs/inline-code'
+import Table from '@editorjs/table'
+
+
+import { isCompositeComponent } from "react-dom/test-utils";
 
 function App() {
 	const [value, setValue] = useState("")
+	const [height, setHeight] = useState(500)
+	const containerRef = useRef()
+
+	const heightChanged = (h) => {
+		if (h === height) return
+
+		setHeight(h)
+
+		if (window.parent) {
+			window.parent.postMessage({
+				message: h,
+				type: 'setHeightCustomField'
+			}, "*")
+		}
+
+	}
 
 	const valueChanged = (val) => {
 
 		if (val === value) return
-
-		console.log("Value changed", value, val)
 
 		setValue(val)
 		if (window.parent) {
@@ -37,44 +59,101 @@ function App() {
 			tools: {
 				header: Header,
 				list: List,
+				inlineCode: {
+					class: InlineCode,
+					shortcut: 'CMD+SHIFT+M',
+				},
+				table: {
+					class: Table,
+				  },
+				image: {
+					class: ImageTool,
+					config: {
+						/**
+						 * Custom uploader
+						 */
+						uploader: {
+							/**
+							 * Upload file to the server and return an uploaded image data
+							 * @param {File} file - file selected from the device or pasted by drag-n-drop
+							 * @return {Promise.<{success, file: {url}}>}
+							 */
+							uploadByFile: (file) => {
+								console.log("UPLOAD FILE", file)
+								// your own uploading logic here
+								// return MyAjax.upload(file).then(() => {
+								// 	return {
+								// 		success: 1,
+								// 		file: {
+								// 			url: 'https://codex.so/upload/redactor_images/o_80beea670e49f04931ce9e3b2122ac70.jpg',
+								// 			// any other image data you want to store, such as width, height, color, extension, etc
+								// 		}
+								// 	};
+								// });
+							},
+
+							/**
+							 * Send URL-string to the server. Backend should load image by this URL and return an uploaded image data
+							 * @param {string} url - pasted image URL
+							 * @return {Promise.<{success, file: {url}}>}
+							 */
+							uploadByUrl: (url) => {
+								console.log("UPLOAD URL", url)
+								// your ajax request for uploading
+								// return MyAjax.upload(file).then(() => {
+								// 	return {
+								// 		success: 1,
+								// 		file: {
+								// 			url: 'https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg',
+								// 			// any other image data you want to store, such as width, height, color, extension, etc
+								// 		}
+								// 	}
+								// })
+							}
+						}
+					}
+				}
 			},
 			onChange: () => {
 
 				editor.save().then(outputValue => {
 					const v = JSON.stringify(outputValue)
 					valueChanged(v)
+					heightChanged(containerRef.current.offsetHeight)
+
 				})
 
-			}
+			},
 
 		});
 
-
-
 		window.addEventListener("message", function (e) {
-			console.log("Got message", e.data)
+
 			//only care about these messages
 			if (e.data.type === 'setInitialValueForCustomField') {
-				console.log("GOT VALUE", e.data.message)
 				if (value !== e.data.message) {
 					setValue(e.data.message)
 
 					editor.isReady.then(() => {
-						console.log("Editor Ready - setting value")
+						//wait for the editor to be ready...
 						const blocks = JSON.parse(e.data.message)
 						editor.render(blocks)
+						this.setTimeout(function () {
+							heightChanged(containerRef.current.offsetHeight)
+						}, 200)
+
 					})
 				}
 			}
 		}, false);
+
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
 
 	return (
-		<div className="field-row" style={{background: "#ebebeb"}}>
-
-			<div id="editorjs">
+		<div style={{ background: "#fff", padding: '0 10px' }}>
+			<div id="editorjs" ref={containerRef}>
 
 			</div>
 		</div>
