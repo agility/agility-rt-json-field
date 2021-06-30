d1eab4870527898914ba56e7008397c097d11522
Joel Varty <joel@agilitycms.com>
2021-06-30 09:44:35 -0400

    Updates

--- custom-field.js
+++ custom-field.js
@@ -627,10 +627,13 @@ var RTJsonField = function() {
 
 		if ($pnl.size() == 0) {
 
+			var row = $(options.$elem).parents(".row")
+			$(".col-lg-8", row).addClass("col-lg-12").removeClass("col-lg-8")
+			$(".col-lg-4", row).addClass("hidden")
+			$(".tab-CONTENT-tab", row).css("padding", 0)
 
-
-			var url = 'https://joelvarty.github.io/agility-rt-json-field/index.html';
-			//USE FOR LOCAL TESTING var url = 'http://localhost:3000';
+			//var url = 'https://joelvarty.github.io/agility-rt-json-field/index.html';
+			var url = 'https://agility-g-editor.netlify.app';
 			var iframe = document.createElement('iframe');
 			iframe.className = "rt-field";
 			iframe.width = '100%';
@@ -671,3 +674,67 @@ var RTJsonField = function() {
 }
 
 ContentManager.Global.CustomInputFormFields.push(new RTJsonField());
+
+
+
+var CloudinaryField = function() {
+    var self = this;
+    self.Label = "Cloudinary Media";
+    self.ReferenceName = "CloudinaryMedia";
+    self.Render = function (options) {
+        /// <summary>Function called whenever the form container this Custom Field Type is rendered or refreshed.</summary>
+        /// <param name="options" type="Object">
+        ///     <field name="$elem" type="jQueryElem">The .field-row jQuery Dom Element.</field>
+        ///     <field name="contentItem" type="ContentItem Object">The entire Content Item object including Values and their KO Observable properties of all other fields on the form.</field>
+        ///     <field name="fieldBinding" type="KO Observable">The value binding of thie Custom Field Type. Get and set this field's value using this property.</field>
+        ///     <field name="fieldSetting" type="Object">Object representing the field's settings such as 'Hidden', 'Label', and 'Description'</field>
+        ///     <field name="readonly" type="boolean">Represents if this field should be readonly or not.</field>
+        /// </param>
+
+		var $pnl = $(".rt-field", options.$elem);
+
+		if ($pnl.size() == 0) {
+
+
+			//var url = 'https://joelvarty.github.io/agility-rt-json-field/index.html';
+			var url = 'https://localhost:3000/cloudinary';
+			var iframe = document.createElement('iframe');
+			iframe.className = "rt-field";
+			iframe.width = '100%';
+			iframe.height = '500px';
+			iframe.src = url
+			iframe.onload = function() {
+
+				iframe.contentWindow.postMessage({
+					message: ko.unwrap(options.fieldBinding),
+					type: 'setInitialValueForCustomField'
+				}, url)
+			}
+			options.$elem.html(iframe);
+
+			window.addEventListener("message", function (e) {
+
+				var messageType = e.data.type
+
+				switch (messageType) {
+					case 'setNewValueFromCustomField':
+						options.fieldBinding(e.data.message);
+						break;
+					case 'setHeightCustomField':
+						iframe.height = e.data.message + "px"
+						break;
+
+					default:
+						//do nothing...
+						console.log("not handled", e.data)
+				}
+
+
+			}, false);
+
+		}
+
+    }
+}
+
+ContentManager.Global.CustomInputFormFields.push(new CloudinaryField());
